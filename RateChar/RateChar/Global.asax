89cfa4751f8241c38cf94f4ceda75e9ab6f7dc29
MBODM <webmaster@mbodm.com>
2016-07-26 14:45:15 +0200

    Re-Added all files

---
+++ RateChar/RateChar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MBODM.RateChar.MvcApplication" Language="C#" %>
---
+++ RateChar/RateChar/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MBODM.RateChar
+{
+    public class BaseViewModel
+    {
+        public bool HasError { get; set; }
+
+        public string ErrorText { get; set; }
+    }
+}
